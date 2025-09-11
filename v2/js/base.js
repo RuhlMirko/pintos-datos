fa312ed6b8f20a8e9f6ffe44cf97678c060ce31e
Mirko Ruhl <ruhlmirkojoel@gmail.com>
2025-09-10 21:23:56 -0300

    Proper results show up after searching
    Results now show up after entering a valid input

--- v2/js/base.js
+++ v2/js/base.js
@@ -1,7 +1,9 @@
 import DATASET from "./dataset.js";
 
 const recordAmountElement = document.getElementById("record-amount");
+const searchBtn = document.getElementById("searchBtn");
 const searchInput = document.getElementById("searchInput");
+
 const logoutBtn = document.getElementById("logout");
 
 logoutBtn.addEventListener("click", function logout() {
@@ -10,6 +12,38 @@ logoutBtn.addEventListener("click", function logout() {
 
 recordAmountElement.textContent = `Registros cargados: ${DATASET.length}`;
 
-searchInput.addEventListener("input", function () {
-  console.log("update");
+searchBtn.addEventListener("click", function () {
+  const resultsContainer = document.getElementById("results");
+
+  resultsContainer.innerHTML = ""; // Clear previous results
+
+  const query = searchInput.value.trim().toLowerCase();
+  if (!query) {
+    resultsContainer.innerHTML =
+      "<h2>Por favor ingrese un número de local para buscar.</h2>";
+    resultsContainer.style.display = "block";
+    resultsContainer.style.backgroundColor = "#a52a17";
+    return;
+  }
+
+  const foundLocal = DATASET.find(
+    (local) => String(local.localNumber) === query
+  );
+  resultsContainer.style.display = "block";
+  resultsContainer.style.backgroundColor = "#464646";
+  if (foundLocal) {
+    resultsContainer.innerHTML = `
+    <h2> Información del Local: ${foundLocal.localNumber}</h2>
+        <div>
+            <p>N° Local: <em>${foundLocal.localNumber}</em> </p>
+            <p>Direccion: <em>${foundLocal.direccion}</em> </p>
+            <p>Barrio: <em>${foundLocal.barrio}</em> </p>
+            <p>Delegado: <em>${foundLocal.delegado}</em> </p>
+            <p>Zona: <em>${foundLocal.zona}</em> </p>
+            <p>Gerente regional: <em>${foundLocal.gerente}</em> </p>
+        </div>
+          `;
+  } else {
+    resultsContainer.innerHTML = `<h2>No se encontró el local con el número "${query}".</h2>`;
+  }
 });
