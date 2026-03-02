6f6912fe8845a648f9596d4ca874f9dec7de2acc
Mirko Ruhl <ruhlmirkojoel@gmail.com>
2026-03-02 14:01:18 -0300

    Added labels

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,12 @@
+import React from "react"
+
 function App() {
+  const [currFilter, setCurrFilter] = React.useState(null)
+
+  function updateFilter(chosenFilter){
+    setCurrFilter(chosenFilter)
+  }
+
   return (
     <main className='search-card'>
       <h1>CONSULTA RÁPIDA</h1>
@@ -6,19 +14,30 @@ function App() {
       <form>
         <label className='search-input-wrapper'>
           <span className='search-icon' aria-hidden='true'>⌕</span>
-          <input type='text' placeholder='Busca por PLU o nombre...' />
+          <label htmlFor="search">PLU / Nombre</label>
+          <input id="search" type='text' placeholder='Busca por PLU o nombre...' />
         </label>
-        <button className='search-btn' type='submit'>CONSULTAR</button>
+        <h2>Filtros</h2>
         <div className='filters'>
-          <button type='button'>
+          <button
+            className={currFilter === 'Fruta' ? 'selected' : ''}
+            type='button'
+            onClick={() => updateFilter('Fruta')}
+          >
             <span className='filter-icon' aria-hidden='true'>🍎</span>
             FRUTAS
           </button>
-          <button type='button'>
+          <button
+            className={currFilter === 'Verdura' ? 'selected' : ''}
+            type='button'
+            onClick={() => updateFilter('Verdura')}
+          >
             <span className='filter-icon' aria-hidden='true'>🍃</span>
             VERDURAS
           </button>
+          
         </div>
+        <button className='search-btn' type='submit'>CONSULTAR</button>
       </form>
     </main>
   )
